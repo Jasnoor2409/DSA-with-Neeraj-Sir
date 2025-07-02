93806c3ed98e767a69021420e49901080740b65c
Jasnoor2409 <jasnoor2409@gmail.com>
2025-07-02 21:04:52 +0530

    sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cout << "enter first no.";
+    cin >> x;
+    cout << "enter second no.";
+    cin >> y;
+    int sum = x+y;
+    cout << sum;
+    return 0;
+}
