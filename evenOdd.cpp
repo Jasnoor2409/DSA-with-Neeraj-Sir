36735fa41078c516f01883b3d48b69d3010698f0
Jasnoor2409 <jasnoor2409@gmail.com>
2025-07-02 21:06:17 +0530

    evenOdd.cpp

---
+++ evenOdd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "enter a no.";
+    cin>>x;
+    if (x%2==0){
+        cout<<x<<" is a even"<<endl;
+    }else{
+        cout<<x<<" is a odd"<<endl;
+    }
+    return 0;
+}
